2219468e530254c573e9614f277504cf2f898aa6
SimoneScapin <simonescap87@gmail.com>
2022-03-25 16:55:48 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
+    <h1>La mia prima app con Vue CLI</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
